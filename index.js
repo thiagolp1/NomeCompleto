29ad8c9a9cd13bbcb9ba0f6f4a1fd289211493b2
Thiago <Thiagolopespereiralp1@gmail.com>
2022-11-11 18:42:25 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = prompt ('Digite seu nome:');
+let sobrenome = prompt ('Digite seu sobrenome:');
+
+const Nomecompleto = confirm (`Seu nome é: ${nome} ${sobrenome}`);
+const Nomedecatalogo = confirm (`Seu nome de catálogo: ${sobrenome.toUpperCase()}, ${nome}`);
